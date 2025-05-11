8b592ef770dd50d76603b835cdc93afff15a9e25
RakulTUD <rsrakul94@gmail.com>
2025-05-11 03:02:06 +0100

    updated app prop file

--- backend-ca2/src/main/java/com/example/ead/be/ServiceController.java
+++ backend-ca2/src/main/java/com/example/ead/be/ServiceController.java
@@ -1,6 +1,70 @@
+// package com.example.ead.be;
+
+// import jakarta.annotation.PostConstruct;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.core.env.Environment;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.Arrays;
+// import java.util.List;
+
+// @RestController
+// public class ServiceController {
+
+// 	@Autowired
+// 	private Environment env;
+
+// 	private Persistence p;
+
+// 	//This method is needed because the application.properties is read AFTER all injections have happened.
+// 	@PostConstruct
+// 	private void postConstruct() {
+// 		System.out.println("******************************************************");
+// 		System.out.println("******************************************************");
+// 		System.out.println("******************************************************");
+// 		System.out.println("env mongoUri:"+env.getProperty("databaseUrl"));
+// 		System.out.println("env dbName:"+env.getProperty("databaseName"));
+// 		System.out.println("env dbName:"+env.getProperty("databaseCollection"));
+// 		System.out.println("******************************************************");
+// 		System.out.println("******************************************************");
+// 		System.out.println("******************************************************");
+
+// 		 p = new Persistence(env.getProperty("databaseUrl"),
+// 				env.getProperty("databaseName"), env.getProperty("databaseCollection"));
+// 	}
+
+// 	@GetMapping("/")
+// 	public String index() {
+// 		p.main2();
+// 		return "Greetings from EAD CA2 Template project 2023-24!";
+// 	}
+
+// 	@GetMapping("/recipes")
+// 	public List<Recipe> getAllRecipes()
+// 	{
+// 		System.out.println("About to get all the recipes in MongoDB!");
+// 		return p.getAllRecipes();
+// 	}
+
+// 	@DeleteMapping("/recipe/{name}")
+// 	private int deleteRecipe(@PathVariable("name") String name)
+// 	{
+// 		System.out.println("About to delete all the recipes named "+name);
+// 		return p.deleteRecipesByName(Arrays.asList(name));
+// 	}
+
+// 	@PostMapping("/recipe")
+// 	@ResponseStatus(HttpStatus.CREATED)
+// 	public int saveRecipe(@RequestBody Recipe rec)
+// 	{
+// 		System.out.println("About to add the following recipe: "+rec);
+// 		return p.addRecipes(Arrays.asList(rec));
+// 	}
+
+// }
 package com.example.ead.be;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,45 +74,30 @@ import java.util.List;
 @RestController
 public class ServiceController {
 
-    @Autowired
-    private Persistence persistence;
+	private final Persistence p = new Persistence(); // Use default constructor that connects to local Mongo
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from EAD CA2 Template project 2023-24!";
-    }
+	@GetMapping("/")
+	public String index() {
+		p.main2();  // Optionally remove this in production
+		return "Greetings from EAD CA2 Template project 2023-24!";
+	}
 
-    @GetMapping("/recipes")
-    public List<Recipe> getAllRecipes() {
-        System.out.println("About to get all the recipes in MongoDB!");
-        return persistence.getAllRecipes();
-    }
+	@GetMapping("/recipes")
+	public List<Recipe> getAllRecipes() {
+		System.out.println("About to get all the recipes in MongoDB!");
+		return p.getAllRecipes();
+	}
 
-    @DeleteMapping("/recipe/{name}")
-    public int deleteRecipe(@PathVariable("name") String name) {
-        System.out.println("About to delete all the recipes named " + name);
-        return persistence.deleteRecipesByName(Arrays.asList(name));
-    }
+	@DeleteMapping("/recipe/{name}")
+	public int deleteRecipe(@PathVariable("name") String name) {
+		System.out.println("About to delete all the recipes named " + name);
+		return p.deleteRecipesByName(Arrays.asList(name));
+	}
 
-    @PostMapping("/recipe")
-    @ResponseStatus(HttpStatus.CREATED)
-    public int saveRecipe(@RequestBody Recipe rec) {
-        System.out.println("=== Recipe Submission Debug ===");
-        System.out.println("Received recipe data: " + rec);
-        System.out.println("Recipe name: " + rec.getName());
-        System.out.println("Ingredients: " + rec.getIngredients());
-        System.out.println("Prep time: " + rec.getPrepTimeInMinutes());
-
-        try {
-            int result = persistence.addRecipes(Arrays.asList(rec));
-            System.out.println("MongoDB operation result: " + result);
-            System.out.println("=== End Recipe Submission Debug ===");
-            return result;
-        } catch (Exception e) {
-            System.err.println("Error saving recipe: " + e.getMessage());
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
+	@PostMapping("/recipe")
+	@ResponseStatus(HttpStatus.CREATED)
+	public int saveRecipe(@RequestBody Recipe rec) {
+		System.out.println("About to add the following recipe: " + rec);
+		return p.addRecipes(Arrays.asList(rec));
+	}
 }
